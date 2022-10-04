94fb84f14ba9ec30133a2e0d60d97782fdacf970
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-10-04 10:09:02 +0200

    Repos sincronizados

---
+++ OpenWebinars/Python3 desde 0/mayor-edad.py
@@ -0,0 +1,8 @@
+#En python no hay que declarar las variables.
+
+edad=int(input("Dime tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+elif edad < 18:
+    print("Eres menor de edad")
+print("Programa terminado")
